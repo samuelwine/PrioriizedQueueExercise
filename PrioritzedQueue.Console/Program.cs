4c7ea63785f0e67429c1f5ee04f67661d100ff23
Shmuel <samuelwine@gmail.com>
2022-02-17 00:45:37 +0000

    Add initial queue and dequeue methods

--- PrioritzedQueue.Console/Program.cs
+++ PrioritzedQueue.Console/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class PurchaseRequestDTO
+{
+}
+
+public class TicketPurchaseQueueItem
+{
+    public DateTime Priority { get; set; }
+    public PurchaseRequestDTO Request { get; set; }
+}
+
+public class PrioritizedQueue
+{
+    public List<TicketPurchaseQueueItem> Queue { get; set; }
+
+    public void Enqueue(TicketPurchaseQueueItem ticketPurchaseQueueItem)
+    {
+        Queue.Add(ticketPurchaseQueueItem);
+    }
+
+    public TicketPurchaseQueueItem Dequeue()
+    {
+        var priorityItem = Queue.OrderBy(x => x.Priority).First();
+        Queue.Remove(priorityItem);
+        return priorityItem;
+    }
+}
