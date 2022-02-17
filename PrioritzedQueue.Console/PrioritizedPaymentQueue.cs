fcfe70cb71439db73e103b5c65f5fa56796c2d58
Shmuel Yaakov <shmuelyaakov@bluecare.express>
2022-02-17 10:41:40 +0000

    Add payment queue

---
+++ PrioritzedQueue.Console/PrioritizedPaymentQueue.cs
@@ -0,0 +1,26 @@
+﻿public class PrioritizedPaymentQueue
+{
+    public List<ProcessPaymentQueueItem> Queue { get; } = new();
+
+    public void Enqueue(ProcessPaymentQueueItem processPaymentQueueItem)
+    {
+        Queue.Add(processPaymentQueueItem);
+    }
+
+    public ProcessPaymentQueueItem Dequeue()
+    {
+        var priorityItem = Queue.OrderBy(x => x.Priority).First();
+        Queue.Remove(priorityItem);
+        return priorityItem;
+    }
+}
+
+public class ProcessPaymentDTO
+{
+}
+
+public class ProcessPaymentQueueItem
+{
+    public int Priority { get; set; }
+    public ProcessPaymentDTO Request { get; set; }
+}
